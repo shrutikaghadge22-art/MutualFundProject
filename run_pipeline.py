9c5b9e97595c16787d526245286128f726854098
shrutikaghadge22-art <shrutikaghadge22@gmail.com>
2026-06-11 21:29:10 +0530

    Added run_pipeline.py

---
+++ run_pipeline.py
@@ -0,0 +1,14 @@
+"""
+Master Pipeline Script
+Bluestock Mutual Fund Analytics Project
+"""
+
+def main():
+    print("Pipeline Started")
+    print("Data Ingestion Completed")
+    print("Performance Analytics Completed")
+    print("Recommendations Generated")
+    print("Pipeline Completed Successfully")
+
+if __name__ == "__main__":
+    main()
